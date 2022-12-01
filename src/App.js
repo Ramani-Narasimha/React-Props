134de765b771c0c877a01cffe6e68cb7b03b6615
Ramani-Narasimha <115544809+Ramani-Narasimha@users.noreply.github.com>
2022-12-01 16:49:22 +0530

    states and events

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 //import ClassComponent from './Class Component'
-import PropsExample from './PropExample'
-
+import FuncProps from  './FuncProps'
+import StateExample from './StateExample'
+import DemoExample from './DemoExample'
+import Events from './Events'
 
 function App(){
   return(
-      <section>
+      <div>
+      <StateExample/>
+      <DemoExample price="1st price"/>
+      <Events/>
+      <FuncProps carname="audi" year="1998"/>
+      <h1>good afternoon</h1>
+      {/*<section>
             <PropsExample name="ramani" age="19"/>
             <PropsExample name="chitti" age="21"/>
 
-           {/* <h1>React.JS</h1>
+           <h1>React.JS</h1>
             <img src="https://miro.medium.com/max/680/1*C4yGTDSLSz86TCakrza2HQ.jpeg" alt="react js"/>
             <p>React is a JavaScript library for building user interfaces.</p>
-             <ClassComponent/>  */} 
+             <ClassComponent/> 
 
-      </section>
+      </section>*/}
+      </div>
+      
     )
 }
 export default App
